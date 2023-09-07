683ad6a1994e1c6814d3e074abbc809ae947e74d
andy <arteysoft@gmail.com>
2023-09-06 21:11:21 -0300

    agregar alumno controller

--- clase18/server/src/main/java/edu/it/model/Alumno.java
+++ clase18/server/src/main/java/edu/it/model/Alumno.java
@@ -1,16 +1,10 @@
 package edu.it.model;
 
-import javax.persistence.*;
-
-@Entity
-@Table(name="alumnos")
 public class Alumno {
-	@Id
     public String id;
     public String nombre;
     public String apellido;
     public String calle;
-    @Column(name="calle_numero")
     public String calleNumero;
     public String estado;
     public String pais;
